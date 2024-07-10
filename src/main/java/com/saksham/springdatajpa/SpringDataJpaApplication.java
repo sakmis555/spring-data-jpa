7d68b6d36b7c69668bd1ed1dfd31aac46f0d992f
Saksham Mishra <sakmis555@gmail.com>
2024-07-10 16:56:24 +0530

    findAll and findById methhods added

--- src/main/java/com/saksham/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/saksham/springdatajpa/SpringDataJpaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
@@ -14,25 +16,29 @@ public class SpringDataJpaApplication {
 
         StudentRepo repo = context.getBean(StudentRepo.class);
 
-        Student s1 = context.getBean(Student.class);
-        Student s2 = context.getBean(Student.class);
-        Student s3 = context.getBean(Student.class);
+//        Student s1 = context.getBean(Student.class);
+//        Student s2 = context.getBean(Student.class);
+//        Student s3 = context.getBean(Student.class);
+//
+//        s1.setRollNo(101);
+//        s1.setName("Ram");
+//        s1.setMarks(45);
+//
+//        s2.setRollNo(102);
+//        s2.setName("Shyam");
+//        s2.setMarks(66);
+//
+//        s3.setRollNo(103);
+//        s3.setName("Hari");
+//        s3.setMarks(28);
 
-        s1.setRollNo(101);
-        s1.setName("Ram");
-        s1.setMarks(45);
+//        repo.save(s1);
+//        repo.save(s2);
+//        repo.save(s3);
 
-        s2.setRollNo(102);
-        s2.setName("Shyam");
-        s2.setMarks(66);
-
-        s3.setRollNo(103);
-        s3.setName("Hari");
-        s3.setMarks(28);
-
-        repo.save(s1);
-        repo.save(s2);
-        repo.save(s3);
+//        System.out.println(repo.findAll());
+        Optional<Student> student = repo.findById(104);
+        System.out.println(student.orElse(new Student()));
 
     }
 
